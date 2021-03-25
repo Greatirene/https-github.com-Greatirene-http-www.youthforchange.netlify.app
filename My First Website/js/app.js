5e0ba439e63d2758fc3a870882c2c4550359d857
Greatirene <79750174+Greatirene@users.noreply.github.com>
2021-03-26 01:24:09 +0200

    Add files via upload

---
+++ My First Website/js/app.js
@@ -0,0 +1,24 @@
+
+alert('Welome');
+
+
+var need = prompt('What information do you want?');
+
+document.getElementById('header').innerHTML = need;
+
+
+function greetings(){
+    var name = prompt('what is your name?');
+    var result = 'Welcome' +' '+ name;
+    document.getElementById('demo').innerHTML = result;
+}
+
+greetings();
+
+    
+
+    var x = alert('Enjoy the content')
+    document.write(x);
+
+
+
